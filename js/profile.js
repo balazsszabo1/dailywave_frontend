17dde3430cc7540c0d3cf3ce461a1bd6fef8962f
diak <diak@mesterlevel.lo>
2025-02-25 11:19:58 +0100

    profilekép

--- js/profile.js
+++ js/profile.js
@@ -63,7 +63,7 @@ async function getProfilPic() {
 
             if (data.profilePicUrl) {
                 const editPic = document.getElementById('profilePic');
-                editPic.style.backgroundImage = `url(${BASE_URL}${data.profilePicUrl}')`;
+                editPic.style.backgroundImage = `url(${BASE_URL}${data.profilePicUrl})`;
             } else {
                 console.log('Profile picture is not set.');
             }
